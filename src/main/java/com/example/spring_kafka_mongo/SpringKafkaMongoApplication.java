6b5a7687b1c636702816dd46260ac6235a113704
Mateus <mateus.dias029@gmail.com>
2025-04-26 13:46:20 -0300

    initial commit

---
+++ src/main/java/com/example/spring_kafka_mongo/SpringKafkaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_kafka_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_kafka_mongo/SpringKafkaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_kafka_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
